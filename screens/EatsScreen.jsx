43337b5c86a0804f66f01cd755e006a83c038c7d
mrparkersson <amoparkerprince78@gmail.com>
2022-08-08 22:24:51 +0000

    Add EatsScreen component

---
+++ screens/EatsScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+
+const EatScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>EatScreen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default EatScreen;
