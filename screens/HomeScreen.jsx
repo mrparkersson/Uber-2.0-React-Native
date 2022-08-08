276dbb701c7417653925923d65cf6b55670922e9
mrparkersson <amoparkerprince78@gmail.com>
2022-08-08 22:25:09 +0000

    Add google places auto complete

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -3,8 +3,16 @@ import { View, Text, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
 //you can write your tailwind styles with the normal styling
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch, useSelector } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
+
+// const MAP_API_KEY = 'AIzaSyAelDLXpQMTcQuY-OM3BZ60jt4JHFirXO4';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+  // const { origin, destination } = useSelector((state) => state.nav);
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -13,6 +21,37 @@ const HomeScreen = () => {
           style={{ width: 100, height: 100 }}
           source={{
             uri: 'https://links.papareact.com/gzs',
+          }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where to?"
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={'search'}
+          enablePoweredByContainer={false}
+          minLength={2}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
           }}
         />
         <NavOptions />
