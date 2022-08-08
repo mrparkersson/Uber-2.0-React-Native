4adc9e8152e6f2c4234fe861d243439c5212e263
mrparkersson <amoparkerprince78@gmail.com>
2022-08-08 11:35:43 +0000

    Add HomeScreen component and install tailwind css

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,11 +1,21 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { View, Text, SafeAreaView, Image } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+//you can write your tailwind styles with the normal styling
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          resizeMode="contain"
+          style={{ width: 100, height: 100 }}
+          source={{
+            uri: 'https://links.papareact.com/gzs',
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
