a7ba8b25bbc3bc688e1f96df6e4b4cddb161e6dd
mrparkersson <amoparkerprince78@gmail.com>
2022-08-08 10:13:36 +0000

    Initial uber clone app commit

---
+++ slices/navSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
+const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+export default navSlice.reducer;
